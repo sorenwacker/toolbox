de0cad4e15fea161d692422199784622925d82fe
Soren <soerendip@gmail.com>
2018-05-18 17:57:36 -0600

    trying

--- setup.py
+++ setup.py
@@ -1,19 +1,49 @@
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+#/usr/bin/env python
+import io, os
+import versioneer
+
+from setuptools import setup
+from os.path import dirname, realpath
+
+with io.open('README.md', encoding='utf8') as readme:
+    long_description = readme.read()
+
+ROOT = dirname(realpath(__file__))
+
+# -----------------------------------------------------------------------------
+# Versioneer
+# -----------------------------------------------------------------------------
+
+versioneer.versionfile_source = 'myToolbox/_version.py'
+versioneer.versionfile_build = 'myToolbox/_version.py'
+versioneer.tag_prefix = '0.1'  # tags are like 1.1.0
+versioneer.parentdir_prefix = 'myToolbox-'  # dirname like 'myproject-1.1.0'
+_version = versioneer.get_version()
+
+
+def package_tree(pkgroot):
+    """ Get list of packages by walking the directory structure and
+    including all dirs that have an __init__.py or are named test.
+    """
+    subdirs = [os.path.relpath(i[0], ROOT).replace(os.path.sep, '.')
+               for i in os.walk(os.path.join(ROOT, pkgroot))
+               if '__init__.py' in i[2]]
+    return subdirs
+
+
+REQUIRES = []
 
 config = {
-    'description': 'My useful python tools.',
+    'description': 'My useful Pyhton tools.',
     'author': 'Soren Wacker',
-    'url': 'https://github.com/soerendip42',
-    'download_url': 'https://github.com/soerendip42/myToolbox',
+    'url': 'github.org',
+    'download_url': 'github.org',
     'author_email': 'swacker@ucalgary.ca',
-    'version': '0.1',
-    'install_requires': [''],
-    'packages': [''],
-    'scripts': [],
-    'name': 'myToolbox'
-}
+    'install_requires': REQUIRES,
+    'packages': package_tree('myToolbox'),
+    'package_data': {'': '*.md'},
+    'name': 'myToolbox',
+    'license': '',
+    'version': _version}
 
 setup(**config)
