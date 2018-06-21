614a5f3043d018b063afa906d03ffcdcd7887673
Soren <soerendip@gmail.com>
2018-06-21 11:19:16 -0600

    changes to notebook

--- myToolbox/jupyter.py
+++ myToolbox/jupyter.py
@@ -1,4 +1,4 @@
 from IPython.core.display import display, HTML
 
 def width(width):
-    display(HTML("<style>.container { width:%d%% !important; }</style>", %width))
+    display(HTML("<style>.container { width:%d%% !important; }</style>" %width))
